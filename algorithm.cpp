5099419faea6c96e29f4a733812778b6580bf18f
Zak <zak_t@hotmail.co.uk>
2024-09-01 18:51:13 +1200

    Minor changes to UC

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,23 +4,29 @@
 #include <unordered_set>
 #include <set>
 #include <algorithm> // For heap functions
-#include <chrono> // For measuring execution time
-#include <ctime> // For std::clock
+#include <chrono>    // For measuring execution time
+#include <ctime>     // For std::clock
 
 using namespace std;
 
-struct Compare {
-    bool operator()(Puzzle* lhs, Puzzle* rhs) {
+struct Compare
+{
+    bool operator()(Puzzle *lhs, Puzzle *rhs)
+    {
         return lhs->getPathLength() > rhs->getPathLength();
     }
 };
 // Function to check if the puzzle is solvable
-bool is_solvable(Puzzle* puzzle) {
+bool is_solvable(Puzzle *puzzle)
+{
     int inversions = 0;
     string board = puzzle->toString();
-    for (int i = 0; i < 9; i++) {
-        for (int j = i + 1; j < 9; j++) {
-            if (board[i] != '0' && board[j] != '0' && board[i] > board[j]) {
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = i + 1; j < 9; j++)
+        {
+            if (board[i] != '0' && board[j] != '0' && board[i] > board[j])
+            {
                 inversions++;
             }
         }
@@ -31,14 +37,15 @@ bool is_solvable(Puzzle* puzzle) {
 //
 // Search Algorithm:  UC with Strict Expanded List
 //
-// Move Generator:  
+// Move Generator:
 //
 ////////////////////////////////////////////////////////////////////////////////////////////
-string uc_explist(string const initialState, string const goalState, int& pathLength, int &numOfStateExpansions, int& maxQLength,
-                               float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions){
-											 
+string uc_explist(string const initialState, string const goalState, int &pathLength, int &numOfStateExpansions, int &maxQLength,
+                  float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions)
+{
+
     // Initialize the path string and start time
-    string path;
+    string path = "";
     clock_t startTime = clock();
 
     // Initialize counters for statistics
@@ -53,29 +60,32 @@ string uc_explist(string const initialState, string const goalState, int& pathLe
     cout << "<<uc_explist>>" << endl;
     cout << "------------------------------" << endl;
 
-
     // Priority queue to store nodes, sorted by path cost
-    priority_queue<Puzzle*, vector<Puzzle*>, Compare> openList;
+    priority_queue<Puzzle *, vector<Puzzle *>, Compare> openList;
 
     // Unordered set to store closed nodes (nodes that have been expanded)
     unordered_set<string> closedList;
 
     // Create initial puzzle node and add it to the open list
-    Puzzle* initialPuzzle = new Puzzle(initialState, goalState);
+    Puzzle *initialPuzzle = new Puzzle(initialState, goalState);
     openList.push(initialPuzzle);
 
+    if (!is_solvable(initialPuzzle))
+    {
+        cout << "No solution found." << endl;
+        return path;
+    }
+
     // Main loop of the search algorithm
-    while (!openList.empty()) {
+    while (!openList.empty())
+    {
         // Get the node with the lowest path cost from the open list
-        Puzzle* currentPuzzle = openList.top();
+        Puzzle *currentPuzzle = openList.top();
         openList.pop();
 
-   if (currentPuzzle == nullptr) {
-        cout << "currentPuzzle is null" << endl;
-        continue;  // Skip this iteration if currentPuzzle is null
-    }
         // If the node has already been expanded, skip it to avoid local loops
-        if (closedList.find(currentPuzzle->toString()) != closedList.end()) {
+        if (closedList.find(currentPuzzle->toString()) != closedList.end())
+        {
             numOfLocalLoopsAvoided++;
             delete currentPuzzle;
             continue;
@@ -86,7 +96,8 @@ string uc_explist(string const initialState, string const goalState, int& pathLe
         numOfStateExpansions++;
 
         // If the node is the goal node, set the path and path length and break the loop
-        if (currentPuzzle->goalMatch()) {
+        if (currentPuzzle->goalMatch())
+        {
             path = currentPuzzle->getPath();
             pathLength = currentPuzzle->getPathLength();
             delete currentPuzzle;
@@ -95,13 +106,17 @@ string uc_explist(string const initialState, string const goalState, int& pathLe
 
         // Generate successors of the current node by moving the empty tile up, right, down, and left
         // Add each successor to the open list
-        if (currentPuzzle->canMoveUp()) openList.push(currentPuzzle->moveUp());
-        if (currentPuzzle->canMoveRight()) openList.push(currentPuzzle->moveRight());
-        if (currentPuzzle->canMoveDown()) openList.push(currentPuzzle->moveDown());
-        if (currentPuzzle->canMoveLeft()) openList.push(currentPuzzle->moveLeft());
+        if (currentPuzzle->canMoveUp())
+            openList.push(currentPuzzle->moveUp());
+        if (currentPuzzle->canMoveRight())
+            openList.push(currentPuzzle->moveRight());
+        if (currentPuzzle->canMoveDown())
+            openList.push(currentPuzzle->moveDown());
+        if (currentPuzzle->canMoveLeft())
+            openList.push(currentPuzzle->moveLeft());
 
         // Update the maximum length of the open list
-        //maxQLength = max(maxQLength, (int)openList.size());
+        // maxQLength = max(maxQLength, (int)openList.size());
         maxQLength = (int)openList.size() > maxQLength ? (int)openList.size() : maxQLength;
 
         // Delete the current node to free up memory
@@ -111,52 +126,44 @@ string uc_explist(string const initialState, string const goalState, int& pathLe
     // Calculate the actual running time of the search algorithm
     actualRunningTime = ((float)(clock() - startTime) / CLOCKS_PER_SEC);
 
-        if (path.empty()) {
+    if (path.empty())
+    {
         cout << "No solution found." << endl;
     }
 
-
     return path;
 }
-
-
-
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
 // Search Algorithm:  A* with the Strict Expanded List
 //
-// Move Generator:  
+// Move Generator:
 //
 ////////////////////////////////////////////////////////////////////////////////////////////
-string aStar_ExpandedList(string const initialState, string const goalState, int& pathLength, int &numOfStateExpansions, int& maxQLength,
-                               float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions, heuristicFunction heuristic){
-											 
-   string path;
-   clock_t startTime;
-   
-   numOfDeletionsFromMiddleOfHeap=0;
-   numOfLocalLoopsAvoided=0;
-   numOfAttemptedNodeReExpansions=0;
+string aStar_ExpandedList(string const initialState, string const goalState, int &pathLength, int &numOfStateExpansions, int &maxQLength,
+                          float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions, heuristicFunction heuristic)
+{
 
+    string path;
+    clock_t startTime;
+
+    numOfDeletionsFromMiddleOfHeap = 0;
+    numOfLocalLoopsAvoided = 0;
+    numOfAttemptedNodeReExpansions = 0;
 
     // cout << "------------------------------" << endl;
     // cout << "<<aStar_ExpandedList>>" << endl;
     // cout << "------------------------------" << endl;
-	actualRunningTime=0.0;	
-	startTime = clock();
-	srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
-	maxQLength= rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
-	numOfStateExpansions = rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
+    actualRunningTime = 0.0;
+    startTime = clock();
+    srand(time(NULL));                   // RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
+    maxQLength = rand() % 200;           // AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
+    numOfStateExpansions = rand() % 200; // AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
 
-
-	
-	
-//***********************************************************************************************************
-	actualRunningTime = ((float)(clock() - startTime)/CLOCKS_PER_SEC);
-	path = "DDRRLLLUUURDLUDURDLUU"; //this is just a dummy path for testing the function
-	pathLength = path.size();
-	return path;		
-		
+    //***********************************************************************************************************
+    actualRunningTime = ((float)(clock() - startTime) / CLOCKS_PER_SEC);
+    path = "DDRRLLLUUURDLUDURDLUU"; // this is just a dummy path for testing the function
+    pathLength = path.size();
+    return path;
 }
-
