12255f207d0d64249c1e445c8536bbe9873b323f
Zak <zak_t@hotmail.co.uk>
2024-09-01 19:53:09 +1200

    Added A* working functions, finally

--- puzzle.h
+++ puzzle.h
@@ -3,80 +3,79 @@
 
 using namespace std;
 
+enum heuristicFunction
+{
+    misplacedTiles,
+    manhattanDistance
+};
 
-enum heuristicFunction{misplacedTiles, manhattanDistance};
+class Puzzle
+{
 
-
-class Puzzle{
-        
 private:
-
     string path;
     int pathLength;
     int hCost;
     int fCost;
-    int depth; 
-        
-    int goalBoard[3][3];
-    
-    int x0, y0; //coordinates of the blank or 0-tile
-    
-    int board[3][3];
-    
-public:
-    
-    string strBoard;
-     
+    int depth;
 
-    Puzzle(const Puzzle &p); //Constructor
+    int goalBoard[3][3];
+
+    int x0, y0; // coordinates of the blank or 0-tile
+
+    int board[3][3];
+
+public:
+    string strBoard;
+
+    Puzzle(const Puzzle &p); // Constructor
     Puzzle(string const elements, string const goal);
 
     string getGoalState() const;
-     
+
     void printBoard();
-    
-    int h(heuristicFunction hFunction);   
-    void updateFCost(); 
+
+    int h(heuristicFunction hFunction) const;
+    void updateFCost();
     void updateHCost(heuristicFunction hFunction);
-    void updateDepth(){
-         depth++;
-    }        
-          
-     
+    void updateDepth()
+    {
+        depth++;
+    }
+
     bool goalMatch();
-     string toString();
-    
-    string getString(){
+    string toString();
+
+    string getString()
+    {
         return strBoard;
-     }
-    
+    }
+
     bool canMoveLeft();
     bool canMoveRight();
     bool canMoveUp();
-    bool canMoveDown();  
+    bool canMoveDown();
 
-//----------------------------------------
-//these functions will be useful for Progressive Deepening Search    
-     bool canMoveLeft(int maxDepth);
-     bool canMoveDown(int maxDepth);
-     bool canMoveRight(int maxDepth);
-     bool canMoveUp(int maxDepth);   
-//----------------------------------------
-      
-    Puzzle * moveUp();
-    Puzzle * moveRight();
-    Puzzle * moveDown();
-    Puzzle * moveLeft();
-     
-     
+    //----------------------------------------
+    // these functions will be useful for Progressive Deepening Search
+    bool canMoveLeft(int maxDepth);
+    bool canMoveDown(int maxDepth);
+    bool canMoveRight(int maxDepth);
+    bool canMoveUp(int maxDepth);
+    //----------------------------------------
+
+    Puzzle *moveUp();
+    Puzzle *moveRight();
+    Puzzle *moveDown();
+    Puzzle *moveLeft();
+
     const string getPath();
-    
+
     void setDepth(int d);
     int getDepth();
-    
-    int getPathLength();
-    int getFCost();
-    int getHCost();
-    int getGCost(); 
-    
+
+    int getPathLength() const;
+    int getFCost() const;
+    int getHCost() const;
+    int getGCost() const;
 };
