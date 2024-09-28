11e68a808892d1278ea9cf0b27aa6724ba36604a
Zak <zak_t@hotmail.co.uk>
2024-09-29 11:36:47 +1300

    updated Fuzzylogic.h

--- A2/fuzzylogic.h
+++ A2/fuzzylogic.h
@@ -17,8 +17,8 @@ using namespace std;
 
 /////////////////////////////////////////////////////
 
-#define MAX_NO_OF_INPUTS 5
-#define MAX_NO_OF_INP_REGIONS 5
+#define MAX_NO_OF_INPUTS 1 //Changed for simplified version
+#define MAX_NO_OF_INP_REGIONS 9 //Increased fuzzy sets
 #define MAX_NO_OF_OUTPUT_VALUES 9
 
 #define TOO_SMALL 1e-6
@@ -27,11 +27,12 @@ using namespace std;
 typedef enum {regular_trapezoid, left_trapezoid, right_trapezoid} trapz_type;
 
 //Input parameters
-enum {INPUT_X, INPUT_Y};
-enum {in_theta,in_theta_dot,in_x,in_x_dot};
+// enum {INPUT_X, INPUT_Y};
+// enum {in_theta,in_theta_dot,in_x,in_x_dot};
+enum {INPUT_X};//Simplified to one input
 
 //Fuzzy sets
-enum {in_nl,in_ns,in_ze,in_ps, in_pl};
+enum {in_nvl,in_nl,in_nm,in_ns,in_ze,in_ps,in_pm, in_pl, in_pvl};
 
 //Fuzzy output terms
 enum {out_nvl,out_nl,out_nm, out_ns, out_ze,out_ps, out_pm,out_pl, out_pvl};
@@ -58,7 +59,7 @@ typedef struct {
 } fuzzy_system_rec;
 
 extern fuzzy_system_rec g_fuzzy_system;
-extern float coefficient_A, coefficient_B, coefficient_C, coefficient_D;
+extern float coefficient_A, coefficient_B; //coefficient_C, coefficient_D; Removed for simplified version
 
 //---------------------------------------------------------------------------
 
@@ -76,8 +77,5 @@ float trapz (float x, trapezoid trz);
 float min_of (float values[],int no_of_inps);
 float fuzzy_system (float inputs[],fuzzy_system_rec fz);
 void free_fuzzy_rules (fuzzy_system_rec *fz);
-
-
-
 
 #endif
